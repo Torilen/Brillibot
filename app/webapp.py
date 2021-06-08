d99aa035dfe4031ae14a1392278aefbf5215a148
Aniss Bentebib <abentebib@asmodine.com>
2021-06-08 13:26:20 +0200

    add api parameter

--- app/webapp.py
+++ app/webapp.py
@@ -30,8 +30,8 @@ confInteract = api.model('interact', {
     'keywordsUnlocked': fields.List(fields.Integer, description="Liste des identifiants de mots-clés déjà débloqué par l'utilisateur. Peut être une liste vide []", required=True, example=[0,1,5,4,8]),
 })
 confCreateAgent = api.model('createAgent', {
-    'data': fields.List(fields.String, description="Données de chargement d'un agent donné au format suivant : ligne de personnalité;ids de mots-clés débloqués avec cette query séparés par des |; réponse scriptée à retourner si les mots-clés sont détectés; ids de mots-clés conditionnant la détection d'autres mots-clés séparés par des |\\n etc", required=True, example=["My name is Aniss;;;","I have 23 years old;;;","My job is Data Scientist;;;","My cay has already eaten a dog;0|1|5;Réponse associée à my cat has already eaten a dog;2|3","Souvenir 2;;;"])
-    'model': fields.String(description="Choix du modèle génératif à utiliser (grafbot/gpt3)", required=True, example='grafbot')
+    'data': fields.List(fields.String, description="Données de chargement d'un agent donné au format suivant : ligne de personnalité;ids de mots-clés débloqués avec cette query séparés par des |; réponse scriptée à retourner si les mots-clés sont détectés; ids de mots-clés conditionnant la détection d'autres mots-clés séparés par des |\\n etc", required=True, example=["My name is Aniss;;;","I have 23 years old;;;","My job is Data Scientist;;;","My cay has already eaten a dog;0|1|5;Réponse associée à my cat has already eaten a dog;2|3","Souvenir 2;;;"]),
+    'model': fields.String(description="Choix du modèle génératif à utiliser (grafbot/gpt3)", required=True, example='grafbot'),
 })
 
 
